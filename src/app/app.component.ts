c52b0a8fc4f427894e7feaa8defc77df2a31bac2
Iip Permana <uipaddr@gmail.com>
2023-05-21 06:29:45 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tutorial-auth-v-eleven';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
